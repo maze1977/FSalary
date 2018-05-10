92dc69cd009c59e0e2b14673014ca402ed1443fe
maze1977 <marcozg@bluewin.ch>
2018-05-10 12:05:28 +0200

    add some types. TODO: type should be some sort of enum, not a pure integer.

--- salary-script/src/formulas/monthly-salary.lisp
+++ salary-script/src/formulas/monthly-salary.lisp
@@ -1,1 +1,6 @@
-(deformula monthly (p.salary p.quota)  (* p.salary p.quota))
+(deformula monthly (p.salary p.quota p.nr-of-hours-worked p.type) 
+(  p.type = 1
+                (p.salary * p.quota)
+            = 2
+                (p.nr-of-hours-worked * p.salary)
+ ))
--- salary-script/src/structures.lisp
+++ salary-script/src/structures.lisp
@@ -1,8 +1,9 @@
 
-(defstruct person name age salary nr-of-children quota sold-items
+(defstruct person name age salary nr-of-children quota sold-items type nr-of-hours-worked 
 ) 
 
-(setq marco (make-person :name "marco" :age 40 :salary 6500 :nr-of-children 1 :quota 1 :sold-items 5000))
+(setq marco (make-person :name "marco" :age 41 :salary 6500 :nr-of-children 1 :quota 1 :sold-items 5000 :type 1 :nr-of-hours-worked (* 42 4)))
+(setq cello (make-person :name "cello" :age 40 :salary 45 :nr-of-children 0 :quota 1 :sold-items 5000 :type 2 :nr-of-hours-worked (* 42 4)))
 
-(defstruct  if-then-else attribute condition value-to-check result-value else-value)
+(defstruct result-with-trace traced-expression result)
 
