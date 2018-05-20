f71ae8c11abbb3f0cbe94110ba9a4311a9214b05
maze1977 <marcozg@bluewin.ch>
2018-05-20 16:12:00 +0200

    improve tracing

--- salary-script/src/structures.lisp
+++ salary-script/src/structures.lisp
@@ -5,5 +5,5 @@
 (setq marco (make-person :name "marco" :age 41 :salary 6500 :nr-of-children 1 :quota 1 :sold-items 5000 :type 1 :nr-of-hours-worked (* 42 4)))
 (setq cello (make-person :name "cello" :age 40 :salary 45 :nr-of-children 0 :quota 1 :sold-items 5000 :type 2 :nr-of-hours-worked (* 42 4)))
 
-(defstruct result-with-trace traced-expression result)
+(defstruct result-with-trace trace result)
 
